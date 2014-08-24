c85a8303928226e296d13069c44e1acc6e7be612
andretadeu <andre.tadeu.de.carvalho@gmail.com>
2014-08-24 12:34:44 -0300

    Added Tabs.

--- README.md
+++ README.md
@@ -9,4 +9,18 @@ This application was created for the course 'Developing Data Products' from Cour
 1. Clone this repository in a folder (e.g. ~/ folder): git clone https://github.com/andretadeu/ddp-finalproject
 2. Open R or RStudio and install 'memoise' package: install.packages('memoise')
 3. Run the following command - runApp('path to the cloned repository folder'). For example, if I cloned the project at /home/test, there should be a folder ddp-finalproject there. The command runApp('/home/test/ddp-finalproject/') should run the Slidy project.
-4. The application is fairly simple, since it is a bunch of filters, a data table and some mean calculation being performed.
+
+## How to use the application
+
+The application is about **mtcars** dataset. Here I filter the cars by selecting several characteristics:
+  * Number of cylinders
+  * Number of gears
+  * Transmission Type
+  * Number of carburetors
+
+This application filters out the irrelevant content and calculate the mean of:
+  * Miles per Gallon
+  * Horsepower
+  * Time for 1/4 mile in seconds
+  * Weight.
+If the filters results in an empty dataset, it will return the full dataset to avoid errors.
--- ui.R
+++ ui.R
@@ -1,38 +1,45 @@
 library(shiny)
 
 shinyUI(
-    pageWithSidebar(
-        headerPanel('Motor Trend of Cars statistics'),
-        sidebarPanel(
-            radioButtons('cyls', 'Cylinders',
-                         c('4 cylinders' = '4',
-                           '6 cylinders' = '6',
-                           '8 cylinders' = '8'),
-                         selected = '4'),
-            checkboxGroupInput('gears', 'Number of gears',
-                               c('3 gears' = '3',
-                                 '4 gears' = '4',
-                                 '5 gears' = '5'),
-                               selected = c('3', '4', '5')),
-            checkboxGroupInput('transmType', 'Transmission type',
-                               c('manual' = '0',
-                                 'automatic' = '1'),
-                               selected = c('0', '1')),
-            sliderInput('carb', 'Number of carburetors',
-                        min = 1, max = 8, step = 1, value = c(1,8)),
-            submitButton(text = 'Search')
-            ),
-        mainPanel(
-            dataTableOutput('filteredMTCars'),
-            br(),
-            h4('Mean MPG'),
-            verbatimTextOutput('meanMpg'),
-            h4('Mean HP'),
-            verbatimTextOutput('meanHP'),
-            h4('Mean time for 1/4 mile'),
-            verbatimTextOutput('meanQSec'),
-            h4('Mean weight'),
-            verbatimTextOutput('meanWeight')
-            )
+    navbarPage(
+        'Motor Trend of Cars',
+        tabPanel('About',
+                 includeMarkdown('README.md')),
+        tabPanel('Application',
+                 pageWithSidebar(
+                     headerPanel('Motor Trend of Cars statistics'),
+                     sidebarPanel(
+                         radioButtons('cyls', 'Cylinders',
+                                      c('4 cylinders' = '4',
+                                        '6 cylinders' = '6',
+                                        '8 cylinders' = '8'),
+                                      selected = '4'),
+                         checkboxGroupInput('gears', 'Number of gears',
+                                            c('3 gears' = '3',
+                                              '4 gears' = '4',
+                                              '5 gears' = '5'),
+                                            selected = c('3', '4', '5')),
+                         checkboxGroupInput('transmType', 'Transmission type',
+                                            c('manual' = '0',
+                                              'automatic' = '1'),
+                                            selected = c('0', '1')),
+                         sliderInput('carb', 'Number of carburetors',
+                                     min = 1, max = 8, step = 1, value = c(1,8)),
+                         submitButton(text = 'Search')
+                     ),
+                     mainPanel(
+                         dataTableOutput('filteredMTCars'),
+                         br(),
+                         h4('Mean MPG'),
+                         verbatimTextOutput('meanMpg'),
+                         h4('Mean HP'),
+                         verbatimTextOutput('meanHP'),
+                         h4('Mean time for 1/4 mile'),
+                         verbatimTextOutput('meanQSec'),
+                         h4('Mean weight'),
+                         verbatimTextOutput('meanWeight')
+                     )
+                 )
         )
     )
+)
